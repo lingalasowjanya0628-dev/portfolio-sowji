77688ea8b4af6e0df06d0bd4f649e4b13c7004b0
lingalasowjanya0628-dev <lingalasowjanya0628@gmail.com>
2026-08-27 16:33:24 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
 alert("Welcome to Sowjanya's Portfolio!");
 
-document.querySelectorAll("a").forEach(link => {
+document.querySelectorAll("a").forEach(function(link) {
     link.addEventListener("click", function() {
         console.log("You clicked: " + this.textContent);
     });
